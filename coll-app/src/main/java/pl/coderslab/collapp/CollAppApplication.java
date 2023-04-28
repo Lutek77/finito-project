c703d6f4ce4f51195395ebcc877f8fcce4a7dd55
Lutek77 <patryk.stachera@gmail.com>
2023-04-28 20:06:49 +0200

    Start

---
+++ coll-app/src/main/java/pl/coderslab/collapp/CollAppApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.collapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollAppApplication.class, args);
+	}
+
+}
---
+++ coll-app/src/test/java/pl/coderslab/collapp/CollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.collapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
