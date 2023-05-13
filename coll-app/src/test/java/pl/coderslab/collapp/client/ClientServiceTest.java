4cc8a55385b9fd2e5232f73f2c53cef0443c772d
Lutek77 <patryk.stachera@gmail.com>
2023-05-13 09:48:41 +0200

    add

--- coll-app/src/test/java/pl/coderslab/collapp/client/ClientServiceTest.java
+++ coll-app/src/test/java/pl/coderslab/collapp/client/ClientServiceTest.java
@@ -3,6 +3,8 @@ package pl.coderslab.collapp.client;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -15,8 +17,8 @@ class ClientServiceTest {
 
 /*    @BeforeEach
     void setUp() {
-        clientRepository = mock(ClientRepository.class);
-        service = new ClientService(clientRepository);
+        repository = mock(ClientRepository.class);
+        service = new ClientService(repository);
     }*/
 
     @Test
@@ -28,15 +30,15 @@ class ClientServiceTest {
     @Test
     void addClient() {
 
-/*        // given
-        ClientDTOTwo clientDTOTwo = new ClientDTOTwo(15L, "Ala", "Kot",
-                "79010112345", "Otwock", "ala@wp.pl", 609100890 );
-        when(repository.save(clientDTOTwo)).thenReturn(clientDTOTwo);
+        // given
+/*        ClientDTOTwo client = new ClientDTOTwo  (15L, "Ala", "Kot",
+                "79010112345", "Otwock", "ala@wp.pl", 609100890  );
+        when(repository.save(client)).thenReturn(client);
         // when
-        ClientDTOTwo result = service.addClient(clientDTOTwo);
+        ClientDTOTwo result = service.addClient(client);
         // then
         assertNotNull(result);
-        assertEquals(clientDTOTwo.getId(), result.getId());*/
+        assertEquals(client.getId(), result.getId());*/
 
     }
 
